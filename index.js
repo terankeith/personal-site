a5873d7b7c45acfe89b522a86aaed0ad9995b381
Teran Keith <terankeith33@gmail.com>
2022-08-13 17:07:01 -0500

    sets up basic node server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("Hello World")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+	console.log(`Server is listening on ${port}`)
+})
